0cbcbf15d846b3dee77b5e4060e0810286abdb7b
Ayesha116 <ayeshajawed1168@gmail.com>
2020-06-12 18:16:01 +0500

    addinga assignment3 of bootcamp on git

---
+++ src/Name.js
@@ -0,0 +1,9 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+function Name(props) {
+  return <span>This is {props.name}!</span>;
+}
+
+export default Name;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Name from './Name';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div>hello <strong><Name name = "Ayesha"></Name></strong>
+  <br/>
+  <h1 className =  "bgcolor">Bootcamp assignment 3</h1>
+  <p>we have learned so far:-</p>
+  <ul>
+    <li>Surge</li>
+    <li>Yarn</li>
+    <li>Actions</li>
+  </ul>
+  <p>total Days = {7+7+3}</p>
+  </div>,
   document.getElementById('root')
 );
 
